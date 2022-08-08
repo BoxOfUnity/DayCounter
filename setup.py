8a05cfc925564435c8fad43006e342ac02141ed9
BoxOfUnity <69089105+BoxOfUnity@users.noreply.github.com>
2022-08-07 22:22:04 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "discord.py",
+    version = "1.7.3",
+    description = "",
+    packages = find_packages("discord.py"),
+    install_requires = ['discord.py']  # Example of external package
+)
